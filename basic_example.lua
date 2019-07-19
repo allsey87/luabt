461739e51f7bf9de1b61f2dc15f296315daec335
Michael Allwright <allsey87@gmail.com>
2019-07-19 11:48:57 +0200

    Add library and a basic example

---
+++ basic_example.lua
@@ -0,0 +1,50 @@
+-- load module
+luabt = require('luabt')
+
+-- global location and target vectors
+location = {x = 0, y = 0}
+target = {x = 10, y = 10}
+
+-- define a walker behavior tree
+walker_root_node = {
+   type = "sequence",
+   children = {
+      -- action leaf, print the current location to the output
+      function()
+         local str = string.format("location = %.2f, %.2f", location.x, location.y)
+         print(str)
+      end,
+      {
+         type = "selector",
+         children = {
+            -- condition leaf, is our location within a threshold of the target
+            function()
+               if math.abs(location.x - target.x) < 0.005 and 
+                  math.abs(location.y - target.y) < 0.005 then
+                  return false, true
+               else
+                  return false, false
+               end
+            end,
+            -- action leaf, move towards target
+            function()
+               -- calculate error
+               local err = {
+                  x = target.x - location.x,
+                  y = target.y - location.y,
+               }
+               -- move
+               location.x = location.x + 1.5 * err.x
+               location.y = location.y + 1.5 * err.y
+               return true
+            end,
+         }
+      }
+   }
+}
+ 
+-- instantiate a behavior tree
+walker_bt = luabt.create(walker_root_node)
+
+-- tick the behavior tree until it has finished (running == false)
+while walker_bt() do end
---
+++ luabt.lua
@@ -0,0 +1,55 @@
+-- create a table for the module
+local luabt = {}
+
+-- define the create function
+function luabt.create(node)
+   -- execution node
+   if type(node) == "function" then
+      return node
+   -- control flow node
+   elseif type(node) == "table" then
+      local children = {}
+      -- recursively construct child nodes
+      for index, child in ipairs(node.children) do
+         children[index] = luabt.create(child)
+      end
+      if node.type == "sequence" then
+         -- return a sequence control flow node
+         return function()
+            for index, child in ipairs(children) do
+               running, success = child()
+               if running then
+                  return true -- child running
+               elseif success == false then
+                  return false, false -- child not running, failed
+               end
+            end
+            return false, true -- not running, all children succeeded
+         end
+      elseif node.type == "selector" then
+         -- return a selector control flow node
+         return function()
+            for index, child in ipairs(children) do
+               running, success = child()
+               if running then
+                  return true -- child running
+               elseif success == true then
+                  return false, true -- child not running, succeeded
+               end
+            end
+            return false, false -- not running, all children failed
+         end
+      end
+   end
+end
+
+-- return the module
+return luabt;
+
+
+
+
+
+
+
+
